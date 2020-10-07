586077664dcc596400c6d40eccff4ca34fff4cb8
OneDeMillion <knittinbrit@gmail.com>
2020-10-07 17:23:59 -0500

    updatejs

--- js/main.js
+++ js/main.js
@@ -10,6 +10,18 @@ const links = [
     {
         label: "Week 3",
         url: "week3/index.html"
+    },
+    {
+        label: "Week 4",
+        url: "week4/index.html"
+    },
+    {
+        label: "Week 5",
+        url: "week5/index.html"
+    },
+    {
+        label: "Week 6",
+        url: "week6/index.html"
     }
 ]
 
